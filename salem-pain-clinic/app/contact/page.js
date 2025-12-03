40852298f8fae09eceb4c86e2ed6c81cd7a0fdc5
Arvinth777 <arvinth273@gmail.com>
2025-12-03 22:15:15 +0530

    fix: update Google Maps embed with correct location and place ID

--- salem-pain-clinic/app/contact/page.js
+++ salem-pain-clinic/app/contact/page.js
@@ -322,7 +322,7 @@ export default function Contact() {
             transition={{ duration: 0.6, delay: 0.2 }}
           >
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3907.8547!2d78.1407!3d11.6806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sSalem%20Pain%20and%20Palliative%20Care%20Clinic!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3907.212856636567!2d78.12620907478987!3d11.679321741891794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf1f588bc086f%3A0x77d230e475f745d9!2sSalem%20pain%20and%20palliative%20care%20clinic!5e0!3m2!1sen!2sin!4v1764780266892!5m2!1sen!2sin"
               width="100%"
               height="500"
               style={{ border: 0 }}
